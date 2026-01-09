93eb992e0aa0d71fb5f28426872d54484cd54d8d
Oluwadamilola Salako <130100824+damozycodes@users.noreply.github.com>
2026-01-09 19:42:23 +0100

    Data set
    Gold bid dataset to train the model

---
+++ Goldfetch.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import dukascopy_python
+from dukascopy_python.instruments import INSTRUMENT_FX_METALS_XAU_USD
+
+# Define the start and end dates
+# Note: Dukascopy data is in UTC
+start_time = datetime(2019, 2, 16)
+end_time = datetime(2025, 11, 30)
+
+# Define the instrument (Gold/USD), interval (4 hour), and offer side (BID price)
+instrument = INSTRUMENT_FX_METALS_XAU_USD
+interval = dukascopy_python.INTERVAL_HOUR_4
+offer_side = dukascopy_python.OFFER_SIDE_BID
+
+# Fetch the data
+df = dukascopy_python.fetch(
+    instrument,
+    interval,
+    offer_side,
+    start_time,
+    end_time,
+)
+
+# Print the first few rows of the data (a pandas DataFrame)
+print(df.head(10))
+
+# You can save the results to a file, for example, a CSV file
+df.to_csv("gold_data_XAUUSD_H4.csv")
